eb92d39552469cb03fc22d9240114abb27aa738c
Witold Dyrka <wdyrka@gmail.com>
2017-03-24 19:29:34 +0100

    pypi release setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'quantiprot',
+  packages = ['quantiprot'],
+  version = '0.2.1',
+  description = 'Quantitative characteristics of protein sequences',
+  author = 'Witold Dyrka, Bogumil M. Konopka, Marta Marciniak',
+  author_email = 'witold.dyrka@pwr.edu.pl',
+  url = 'https://git.e-science.pl/wdyrka/quantiprot',
+  download_url = 'https://git.e-science.pl/wdyrka/quantiprot/repository/archive.tar.gz?ref=0.2.1',
+  keywords = ['protein', 'sequence', 'quantitative', 'analysis', 'rqa', 'n-gram'],
+  classifiers = [],
+)
