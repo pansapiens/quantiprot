786c104aa4104014479c426c71b6f7362d84aafc
Witold Dyrka <wdyrka@gmail.com>
2017-03-27 17:28:46 +0200

    bugfix: data included in package; ver. 0.2.3

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'quantiprot',
+  version = '0.2.3',
   packages = ['quantiprot','quantiprot.utils','quantiprot.metrics','quantiprot.analysis'],
-  version = '0.2.2',
+  package_data = { 'quantiprot.metrics': [ 'data/aaindex1'] },
   description = 'Quantitative characteristics of protein sequences',
   author = 'Witold Dyrka, Bogumil M. Konopka, Marta Marciniak',
   author_email = 'witold.dyrka@pwr.edu.pl',
   url = 'https://git.e-science.pl/wdyrka/quantiprot',
-  download_url = 'https://git.e-science.pl/wdyrka/quantiprot/repository/archive.tar.gz?ref=0.2.2',
+  download_url = 'https://git.e-science.pl/wdyrka/quantiprot/repository/archive.tar.gz?ref=0.2.3',
   keywords = ['protein', 'sequence', 'quantitative', 'analysis', 'rqa', 'n-gram'],
   classifiers = [],
   install_requires = ['numpy>=1.11.0', 'requests>=2.10.0'],
